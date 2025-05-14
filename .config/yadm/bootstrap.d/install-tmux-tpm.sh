2c601e79c6298d1eb359cd95c1b95c87e7c60dd2
Kohei Wada <program3152019@gmail.com>
2025-05-14 23:51:37 +0900

    fix(tmux): change TPM install path to XDG-compliant location
    Updated tmux plugin manager (TPM) install path from ~/.tmux to
    ~/.local/share/tmux/plugins for XDG compliance. Modified both
    tmux.conf and install script to reflect this change.

--- .config/yadm/bootstrap.d/install-tmux-tpm.sh
+++ .config/yadm/bootstrap.d/install-tmux-tpm.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-if [ -d ~/.tmux/plugins/tpm ]; then
+readonly TPM_PATH="$HOME/.local/share/tmux/plugins/tpm"
+
+if [ -d "$TPM_PATH" ]; then
     echo "tmux plugin manager already installed"
 else
     echo "installing tmux plugin manager"
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    git clone https://github.com/tmux-plugins/tpm "$TPM_PATH"
 fi
