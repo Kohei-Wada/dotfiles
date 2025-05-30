377fb5f03bd879590ac6e21d5f13f6c007c0f763
Kohei Wada <program3152019@gmail.com>
2025-05-30 13:13:01 +0900

    feat(options): enable modeline and set modelines to 5
    Enabled the modeline feature and set modelines to 5 for better support of
    Vim modelines. Also aligned indentation for display-related options for
    consistency.

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -9,13 +9,15 @@ opt.swapfile = false       -- Do not create swap files
 opt.modifiable = true      -- Make buffer modifiable
 
 -- Display settings
-opt.number = true         -- Show current line number
-opt.relativenumber = true -- Show relative line numbers
-opt.cmdheight = 2         -- Set command line height to 2 lines
-opt.laststatus = 2        -- Always show status line
-opt.signcolumn = "yes:1"    -- Always show sign column to prevent width changes
+opt.number = true            -- Show current line number
+opt.relativenumber = true    -- Show relative line numbers
+opt.cmdheight = 2            -- Set command line height to 2 lines
+opt.laststatus = 2           -- Always show status line
+opt.signcolumn = "yes:1"     -- Always show sign column to prevent width changes
 opt.cursorlineopt = "number" -- Highlight current line number
-opt.cursorline = true     -- Highlight current line
+opt.cursorline = true        -- Highlight current line
+opt.modeline = true          -- Show modeline at the bottom of the file
+opt.modelines = 5        -- Set number of modelines to check
 
 -- Indentation settings
 opt.tabstop = 4      -- Set tab width to 4 spaces
