232f78c559c4cc210d0cf181ecb3ce8e736ecae2
Kohei Wada <program3152019@gmail.com>
2025-07-02 23:56:50 +0900

    fix: update note ID generation logic for Obsidian
    Changed the note_id_func to use the title directly as the note ID if
    provided, and fallback to a timestamp with random letters if not. This
    improves note naming consistency and aligns with user expectations.

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -14,25 +14,23 @@ return {
         path = "~/ghq/github.com/Kohei-Wada/Obsidian/github",
         overrides = {
           new_notes_location = "01_FleetingNotes",
+          notes_subdir = "01_FleetingNotes",
           templates = {
             folder = "99_Templates",
           },
 
           note_id_func = function(title)
-            -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
-            -- In this case a note with the title 'My new note' will be given an ID that looks
-            -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
-            local suffix = ""
             if title ~= nil then
-              -- If title is given, transform it into valid file name.
-              suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+              -- If title is not nil, use the title as the note ID.
+              return title
             else
               -- If title is nil, just add 4 random uppercase letters to the suffix.
+              local suffix = ""
               for _ = 1, 4 do
                 suffix = suffix .. string.char(math.random(65, 90))
               end
+              return tostring(os.time()) .. "-" .. suffix
             end
-            return tostring(os.time()) .. "-" .. suffix
           end,
         },
       },
