543617887e6181eac8e5051576151a02408cefe6
Kohei Wada <program3152019@gmail.com>
2025-07-12 17:57:17 +0900

    feat: Add cmdwin config for command-line window
    Created cmdwin.lua to customize the command-line window behavior. Line numbers
    and sign column are disabled for better visibility. Also, quit commands are
    removed from command history to prevent accidental exits.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,6 +3,7 @@ require "config.builtin_plugins"
 require "config.command"
 require "config.autocmd"
 require "config.terminal"
+require "config.cmdwin"
 
 -- after all the above, load plugins
 require "config.lazy"
---
+++ .config/nvim/lua/config/cmdwin.lua
@@ -0,0 +1,24 @@
+vim.api.nvim_create_autocmd("CmdwinEnter", {
+  pattern = "*",
+  desc = "Disable line numbers and sign column in command-line window",
+  callback = function()
+    vim.opt_local.number = false
+    vim.opt_local.relativenumber = false
+    vim.opt_local.signcolumn = "no"
+  end,
+})
+
+vim.api.nvim_create_autocmd("CmdwinEnter", {
+  pattern = ":",
+  callback = function()
+    -- Delete quit commands from command history
+    local patterns = {
+      "^qa\\?!\\?$", -- q, qa, q!, qa!
+      "^wq\\?a\\?!\\?$", -- w, wq, wa, wqa, w!, wq!, wa!, wqa!
+    }
+
+    for _, pattern in ipairs(patterns) do
+      vim.cmd("g/" .. pattern .. "/d")
+    end
+  end,
+})
