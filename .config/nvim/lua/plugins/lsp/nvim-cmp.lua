6591178c60d54fae4ee1604f78d0ed67b3f2e5f0
Kohei Wada <program3152019@gmail.com>
2025-07-25 08:36:52 +0900

    refactor: move LuaSnip config to separate file
    Separated LuaSnip plugin configuration from nvim-cmp to improve modularity
    and maintainability. This change makes plugin management clearer and easier
    to update in the future.

--- .config/nvim/lua/plugins/lsp/nvim-cmp.lua
+++ .config/nvim/lua/plugins/lsp/nvim-cmp.lua
@@ -1,23 +1,5 @@
 return {
   {
-    "L3MON4D3/LuaSnip",
-    lazy = true,
-    -- follow latest release.
-    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-    -- install jsregexp (optional!).
-    build = "make install_jsregexp",
-    dependencies = {
-      "saadparwaiz1/cmp_luasnip",
-      "rafamadriz/friendly-snippets",
-    },
-
-    config = function()
-      require("luasnip").setup()
-      require("luasnip.loaders.from_vscode").lazy_load()
-    end,
-  },
-
-  {
     "hrsh7th/nvim-cmp",
     event = { "InsertEnter", "CmdlineEnter" },
     config = function()
