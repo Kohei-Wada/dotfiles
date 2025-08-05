7297cf671c3e2396ca4e999d6d649c2a81e70a76
Kohei Wada <program3152019@gmail.com>
2025-08-06 01:33:13 +0900

    refactor: scope LSP keymaps to buffer on attach
    Updated LSP keymap definitions to use buffer-local options during the
    LspAttach event. This prevents keymap conflicts and ensures mappings are
    only active for relevant buffers.
    o

--- .config/nvim/lua/plugins/lsp/mason.lua
+++ .config/nvim/lua/plugins/lsp/mason.lua
@@ -35,19 +35,19 @@ return {
 
     config = function(_, opts)
       require("mason-lspconfig").setup(opts)
+
       vim.api.nvim_create_autocmd("LspAttach", {
         group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-        callback = function(ev)
-          local tmp = { buffer = ev.buf }
-          vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
-          vim.keymap.set("n", "gd", vim.lsp.buf.definition)
-          vim.keymap.set("n", "K", vim.lsp.buf.hover)
-          vim.keymap.set("n", "gi", vim.lsp.buf.implementation)
-          vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition)
-          vim.keymap.set("n", "gr", vim.lsp.buf.references)
+        callback = function(event)
+          local opt = { buffer = event.buf }
+          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opt)
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opt)
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opt)
+          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opt)
+          vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opt)
           vim.keymap.set("n", "<space>f", function()
             vim.lsp.buf.format { async = true }
-          end, tmp)
+          end, opt)
         end,
       })
 
