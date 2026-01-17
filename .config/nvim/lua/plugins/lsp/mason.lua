188441921f35d832c08b0848a86d0230b90cf9f7
Kohei Wada <program3152019@gmail.com>
2026-01-18 02:37:29 +0900

    chore: update plugin lockfile and Mason config
    Updated several plugin commit hashes in lazy-lock.json to their latest
    versions. Simplified Mason plugin configuration by removing the custom UI
    border option for consistency with default settings.

--- .config/nvim/lua/plugins/lsp/mason.lua
+++ .config/nvim/lua/plugins/lsp/mason.lua
@@ -9,11 +9,7 @@ return {
     cmd = "Mason",
     keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
     build = ":MasonUpdate",
-    opts = {
-      ui = {
-        border = "rounded",
-      },
-    },
+    opts = {},
   },
 
   {
